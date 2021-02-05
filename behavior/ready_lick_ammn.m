a39589e1f016928172fdc85602399e863226c450
Yuriy <shymkivy@gmail.com>
2021-02-05 16:01:53 -0500

    behavior scripts

--- behavior/ready_lick_ammn.m
+++ behavior/ready_lick_ammn.m
@@ -2,17 +2,18 @@
 clear;
 
 %% params
-fname = 'nm_ready_lick_ammn_day5';
+fname = 're_ready_lick_ammn_day4_part4';
 
 ops.paradigm_duration = 1800;  %  sec
 ops.trial_cap = 500;            % 200 - 400 typical with 25sol duration
 
-ops.pre_trial_delay = 2;  % sec
+ops.pre_trial_delay = .5;  % sec
+ops.pre_trial_delay_rand = 0;
 ops.reward_window = 2;
 ops.failure_timeout = 5;
 ops.post_trial_delay = 2;  % sec
 ops.require_second_lick = 1;
-ops.reward_period_flash = 1;
+ops.reward_period_flash = 0;
 
 ops.water_dispense_duration = 0.025;
 
@@ -27,7 +28,7 @@ ops.rand_time_pad = .05;
 ops.stim_range = [3 4 5 6 7];
 ops.red_pre_trial = 3;
 ops.red_post_trial = 3;
-ops.red_lim = 20; % min lim is 5;
+ops.red_lim = 7; % min lim is 5; adjust for pretrial red padding
 ops.MMN_probab=[0.1*ones(1,max(ops.red_lim-4,1)) .2 .25 .5 1]; 
 % MMN_probab=[.01 .01 .02 .1 .1 .1 .1 .5 .5 .5 1];   % jordan's probab
 
@@ -43,12 +44,12 @@ ops.num_freqs = 10;
 ops.increase_factor = 1.5;
 ops.MMN_patterns = [3,6; 4,7; 3,8];
 ops.base_freq = 0.001; % baseline frequency
-ops.modulation_amp = 4 ;
+ops.modulation_amp = 2 ;
 %%
 pwd2 = fileparts(which('ready_lick_ammn.m'));
 
 addpath([pwd2 '\..\auditory_stim\functions']);
-save_path = [pwd2 '\..\..\stim_scripts_output\behavior\' fname];
+save_path = [pwd2 '\..\..\stim_scripts_output\behavior\'];
 circuit_path = [pwd2 '\..\RPvdsEx_circuits\'];
 circuit_file_name = 'sine_mod_play_YS.rcx';
 %% Initialize arduino
@@ -100,6 +101,7 @@ for n_tr = 1:ops.trial_cap
     mmn_red_dev_seq(n_tr,:) = randsample(ops.stim_range, 2, 0);
 end
 
+%figure; histogram(dev_idx);
 %% run paradigm
 RP.Run;
 RP.SetTagVal('ModulationAmp', ops.modulation_amp);
@@ -118,6 +120,7 @@ time_correct_lick = zeros(ops.trial_cap, 1);
 time_stim = cell(ops.trial_cap,1);
 n_trial = 0;
 n_reward = 0;
+start_reward_period = -500;
 while and((now*1e5 - start_paradigm)<ops.paradigm_duration, n_trial<=ops.trial_cap)
     
     % trial available, wait for lick to start
@@ -136,10 +139,16 @@ while and((now*1e5 - start_paradigm)<ops.paradigm_duration, n_trial<=ops.trial_c
         lick = 0;
         start_trial = now*1e5;
         time_trial_start(n_trial) = start_trial - start_paradigm;
-        time_stim{n_tr} = zeros(num_stim,1);
+       
+        trial_delay = ops.pre_trial_delay+ops.pre_trial_delay_rand*rand(1);
+        pause(trial_delay);
+        
         stim_finish = 0;
+        reward_received = 0;
         num_stim = dev_idx(n_trial)+ops.red_post_trial;
+        time_stim{n_tr} = zeros(num_stim,1);
         n_stim = 1;
+        
         % start stim
         while and(~stim_finish, n_stim<=num_stim)
             if n_stim == dev_idx(n_trial)
@@ -151,16 +160,17 @@ while and((now*1e5 - start_paradigm)<ops.paradigm_duration, n_trial<=ops.trial_c
             end
             volt =  stim_type/ops.num_freqs*4;
             
-            % play
-            lick = 0;
-            start_stim = now*1e5;%GetSecs();
             if reward_trial
                 if ops.reward_period_flash
                     session.outputSingleScan([0,0,1,0]); %write(arduino_port, 1, 'uint8'); % turn on LED
                     pause(.005);
                     session.outputSingleScan([0,0,0,0]); %write(arduino_port, 2, 'uint8'); % turn off LED
                 end
-                start_reward_period = now*1e5;
+            end
+            % play
+            start_stim = now*1e5;%GetSecs();
+            if reward_trial
+                start_reward_period = start_stim;
                 time_reward_period_start(n_trial) = start_reward_period - start_paradigm;
             end
             RP.SetTagVal('CarrierFreq', control_carrier_freq(stim_type));
@@ -169,12 +179,15 @@ while and((now*1e5 - start_paradigm)<ops.paradigm_duration, n_trial<=ops.trial_c
             while (now*1e5 - start_stim) < ops.stim_time
                 data_in = inputSingleScan(session);
                 if data_in > ops.lick_thresh
-                    lick = 1;
-                    if (now*1e5 - start_reward_period) < ops.reward_window
-                        time_correct_lick(n_trial) = now*1e5 - start_paradigm;
-                        session.outputSingleScan([0,0,0,1]); % write(arduino_port, 3, 'uint8');
-                        pause(ops.water_dispense_duration);
-                        session.outputSingleScan([0,0,0,0]);
+                    if ~reward_received
+                        if (now*1e5 - start_reward_period) < ops.reward_window
+                            time_correct_lick(n_trial) = now*1e5 - start_paradigm;
+                            reward_received = 1;
+                            session.outputSingleScan([0,0,0,1]); % write(arduino_port, 3, 'uint8');
+                            pause(ops.water_dispense_duration);
+                            session.outputSingleScan([0,0,0,0]);
+                            n_reward = n_reward + 1;
+                        end
                     end
                 end
             end
@@ -183,25 +196,43 @@ while and((now*1e5 - start_paradigm)<ops.paradigm_duration, n_trial<=ops.trial_c
             session.outputSingleScan([0,0,0,0]);
             
             % pause for isi
-            pause(ops.isi_time+rand(1)*ops.rand_time_pad)
+            start_isi = now*1e5;
+            isi_duration = ops.isi_time+rand(1)*ops.rand_time_pad;
+            while (now*1e5 - start_isi) < isi_duration
+                data_in = inputSingleScan(session);
+                if data_in > ops.lick_thresh
+                    if ~reward_received
+                        if (now*1e5 - start_reward_period) < ops.reward_window
+                            time_correct_lick(n_trial) = now*1e5 - start_paradigm;
+                            reward_received = 1;
+                            session.outputSingleScan([0,0,0,1]); % write(arduino_port, 3, 'uint8');
+                            pause(ops.water_dispense_duration);
+                            session.outputSingleScan([0,0,0,0]);
+                            n_reward = n_reward + 1;
+                        end
+                    end
+                end
+            end
             
             time_stim{n_tr}(n_stim) = start_stim-start_paradigm;
             
             n_stim = n_stim  + 1;
         end
-        n_trial = n_trial + 1;
+        fprintf('n_trial = %d, n_reward = %d\n', n_trial, n_reward);
     end
     
-    pause(ops.post_reward_delay);
+    pause(ops.post_trial_delay);
     
 end
-write(arduino_port, 2, 'uint8'); % turn off LED
+session.outputSingleScan([0,0,0,0]);
+RP.Halt;
+%write(arduino_port, 2, 'uint8'); % turn off LED
 
 pause(5);
-session.outputSingleScan([0,3]);
+session.outputSingleScan([0,3,0,0]);
 time_paradigm_end = now*1e5 - start_paradigm;
 pause(1);
-session.outputSingleScan([0,0]);
+session.outputSingleScan([0,0,0,0]);
 pause(5);
 
 %% save data
@@ -217,4 +248,4 @@ file_name = sprintf('%s_%d_%d_%d_%dh_%dm.mat',fname, temp_time(2), temp_time(3),
 if ~exist(save_path, 'dir')
     mkdir(save_path);
 end
-save(save_path, 'trial_data', 'ops');
+save([save_path file_name],  'trial_data', 'ops');
