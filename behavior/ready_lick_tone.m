a700c337f2cdddb0acd03b436b7bef3e9f9edb92
yuriy <shymkivy@gmail.com>
2021-11-20 15:02:12 -0500

    behavior

--- behavior/ready_lick_tone.m
+++ behavior/ready_lick_tone.m
@@ -1,4 +1,8 @@
-% communicates with the "minimal" arduino script
+% teaching paradigm
+% step 1 - train mouse to use lick port "ready_lick_reward"
+% step 2 - train mouse to lick in response to tone "ready_lick_tone"
+
+% no arduino script required, only as power source
 clear;
 
 %% params
@@ -8,6 +12,7 @@ fname = 'mosueR2';
 ops.paradigm_duration = 1800;  %  sec
 ops.trial_cap = 500;            % 200 - 400 typical with 25sol duration
 
+% ------- trial bout params -----------
 ops.initial_stop_lick_period = 0;
 ops.pre_trial_delay = 2;  % sec
 ops.pre_trial_delay_rand = 0;
@@ -23,42 +28,54 @@ ops.water_dispense_duration_small = 0.025;
 ops.reward_lick_rate_thersh_large = .9;          % licks per sec below thresh give reward
 ops.reward_lick_rate_thersh_small = 0;        % licks per sec below thresh give reward1
 
-ops.lick_thresh = 4;
 
-ops.stim_selection_type = 'sequences'; % 'randsamp', 'sequences', 'rand_sequences' 
-ops.MMN_pat = [3, 6];
-ops.num_seq = 50;
+ops.sound_TD_amp = 0; % else use computer sound card (headphone)
+
+% cont and mmn are dev trials fit among other trials
+% quiet is dev trial coming in some time range
 
 % ------ Stim params ------
 ops.stim_time = 0.5;                                         % sec
 ops.isi_time = 0.5;
 ops.rand_time_pad = .05;
 
-% MMN trial params
-ops.stim_range = [3 4 5 6 7];
-ops.red_pre_trial = 3;
-ops.red_post_trial = 3;
+% ----- auditory stim params ------------
+ops.start_freq = 1000;
+ops.end_freq = 18000;
+ops.num_freqs = 10;
+ops.freq_scale = 'log'; % 'log' or 'linear'
+
+% ------ trial structure ------------
+ops.trial_ctx_type = 'quiet'; % 'control', 'mmn', 'quiet';
+
+ops.dev_tone_list = 5; % list of possible 'deviant' tones that get rewarded
+
+% 'mmn' params
+ops.red_tone_list = [3 4 5 6 7]; % possible red tones for mmn
+ops.red_num_pre_trial = 3;
+ops.red_num_post_trial = 3;
 ops.red_lim = 7; % min lim is 5; adjust for pretrial red padding
-ops.MMN_probab=[0.1*ones(1,max(ops.red_lim-4,1)) .2 .25 .5 1]; 
+ops.dev_probab=[0.1*ones(1,max(ops.red_lim-4,1)) .2 .25 .5 1]; 
 % MMN_probab=[.01 .01 .02 .1 .1 .1 .1 .5 .5 .5 1];   % jordan's probab
 
-% ------ Other ------
+% 'quiet' params
+ops.quiet_dev_delay_range = [1 4];
+
+% ----- sequences of trials structure -----------------
+ops.stim_selection_type = 'sequences'; % 'randsamp', 'sequences', 'rand_sequences' 
+ops.MMN_pat = [3, 6];
+ops.seq_len = 50;
+
+
+% ------ Other params ------
 ops.synch_pulse = 1;      % 1 Do you want to use led pulse for synchrinization
 ops.lick_thresh = 4;
 ops.transition_thresh = 4.5;
 
-% ----- auditory stim params ------------
-ops.start_freq = 2000;
-% ops.end_freq = 90000;
-
-ops.num_freqs = 10;
-ops.increase_factor = 1.5;
-ops.MMN_patterns = [3,6; 4,7; 3,8];
+% ----- TD amplifier params-----
 ops.base_freq = 0.001; % baseline frequency
 ops.modulation_amp = 3;
 
-ops.tone_learn = 10000; % kHz
-
 %% Run script
 %s_ready_lick_ammn_core;
-s_ready_lick_tone_headphone_core;
+s_ready_lick_tone_core;
---
+++ behavior/s_run_reward_period.m
@@ -0,0 +1,29 @@
+while (now*86400 - start_reward) < reward_duration
+    data_in = inputSingleScan(session);
+    s_get_lick_state;
+    if lick_transition>0 % if a new lick
+        if ~reward_type(n_trial) % if not rewarded yet continue
+            % is still within reward period
+            if (now*86400 - start_reward_period) < ops.reward_window
+                time_correct_lick(n_trial) = now*86400 - start_paradigm;
+                if reward_onset_lick_rate(n_trial)<ops.reward_lick_rate_thersh_large
+                    reward_type(n_trial) = 3;        % large reward
+                    session.outputSingleScan([volt,0,0,1]); % write(arduino_port, 3, 'uint8');
+                    session.outputSingleScan([volt,0,0,1]);
+                    pause(ops.water_dispense_duration_large);
+                    session.outputSingleScan([volt,0,0,0]);
+                    session.outputSingleScan([volt,0,0,0]);
+                elseif reward_onset_lick_rate(n_trial)<ops.reward_lick_rate_thersh_small
+                    reward_type(n_trial) = 2;        % small reward
+                    session.outputSingleScan([volt,0,0,1]); % write(arduino_port, 3, 'uint8');
+                    session.outputSingleScan([volt,0,0,1]);
+                    pause(ops.water_dispense_duration_small);
+                    session.outputSingleScan([volt,0,0,0]);
+                    session.outputSingleScan([volt,0,0,0]);
+                else
+                    reward_type(n_trial) = 1;        % no reward
+                end
+            end
+        end
+    end
+end
